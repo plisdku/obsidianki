47e050d3071cb656348ef374dc3bf60461f23f36
Paul Hansen <paul.c.hansen@gmail.com>
2025-04-20 14:14:48 -0700

    managed to convert a test file

--- src/obsidianki/splitter.py
+++ src/obsidianki/splitter.py
@@ -1,21 +1,16 @@
 import re
 import pathlib
 
-fname = pathlib.Path("/Users/paul/Documents/Obsidian Vault/Math notes/Atakishiyev, On Classical Orthogonal Polynomials.md").absolute()
-with open(fname) as fh:
-    contents = fh.read()
-
-
 def split_between_matches(contents, matches):
     """
     Split the contents between the matches.
     """
     match_contents = []
-    match_contents.append(contents[:matches[0].start()].strip())
+    match_contents.append(contents[:matches[0].start()])
     for idx in range(len(matches)-1):
-        match_content = contents[matches[idx].end():matches[idx+1].start()].strip()
+        match_content = contents[matches[idx].end():matches[idx+1].start()]
         match_contents.append(match_content)
-    match_contents.append(contents[matches[-1].end():].strip())
+    match_contents.append(contents[matches[-1].end():])
     return match_contents
 
 
@@ -30,35 +25,102 @@ def split_sections(contents):
     matches = list(tag_pattern.finditer(contents))
 
     # Filter the matches to only include valid tags
-    valid_tags = ["Q", "A", "Addendum", "Book", "Page", "Chapter", "Tags"]
+    valid_tags = ["Q", "A", "AA", "Addendum", "Book", "Page", "Chapter", "Tags"]
     matches = [m for m in matches if m.group(2) in valid_tags]
 
     match_contents = split_between_matches(contents, matches)
 
-    return list(zip(matches, match_contents))
-
-# print(split_sections(contents))
-# exit(0)
+    return list(zip(matches, match_contents[1:]))
 
 
-eqn = r"""a three-term recurrence relation of the form
-$$
-x p_n(x) = A_n p_{n+1}(x) + B_n p_n(x) + C_n p_{n-1}(x),
-$$
-where $p_{-1}(x) = 0$, $p_0(x) = 1$, $A_n, B_n, C_n$ are real, and $A_n C_{n+1} > 0$ for $n = 0, 1, 2, 3, \dots$.
-Orthogonality may be defined with respect to a weight function $w(x)$, either on the real line,
-$$
-\int_{-\infty}^\infty p_m(x) p_n(x) w(x) \, dx = 0, \quad m \ne n,
-$$
-or discrete,
-$$
-\sum_{j=0}^\infty p_m(x_j) p_n(x_j) w_j = 0, \quad m \ne n.
-$$
-"""
+def convert_math_delims(section: str) -> str:
+    """
+    Convert math delimiters in the section.
+        - $...$ becomes \(...\)
+        - $$...$$ becomes \[...\]
+        
+    Args:
+        section (str): The section to convert.
+    Returns:
+        str: The converted section.
+    """
 
-delim_pattern = re.compile(r"(\$+|[{}])", re.MULTILINE)
+    delim_pattern = re.compile(r"(\$+|[{}])", re.MULTILINE)
 
-delims = list(delim_pattern.finditer(eqn))
-mm = split_between_matches(eqn, delims)
-print(mm)
+    delims = list(delim_pattern.finditer(section))
 
+    if not delims:
+        return section
+
+    betweens = split_between_matches(section, delims)
+
+    out = []
+    out.append(betweens[0])
+
+    curly_brace_depth = 0
+    math_delim_depth = 0
+
+    for delim_match, between in zip(delims, betweens[1:]):
+        delim = delim_match.group(0)
+        if delim == "$":
+            if curly_brace_depth == 0:
+                if math_delim_depth == 0:
+                    delim = r"\("
+                    math_delim_depth += 1
+                else:
+                    delim = r"\)"
+                    math_delim_depth -= 1
+        elif delim == "$$":
+            if curly_brace_depth == 0:
+                if math_delim_depth == 0:
+                    delim = r"\["
+                    math_delim_depth += 1
+                else:
+                    delim = r"\]"
+                    math_delim_depth -= 1
+        elif delim == "{":
+            curly_brace_depth += 1
+        elif delim == "}":
+            curly_brace_depth -= 1
+        else:
+            assert "unexpected token"
+        
+        out.append(delim)
+        out.append(between)
+
+    return "".join(out)
+
+
+fname = pathlib.Path("/Users/paul/Documents/Obsidian Vault/Math notes/Atakishiyev, On Classical Orthogonal Polynomials.md").absolute()
+with open(fname) as fh:
+    contents = fh.read()
+
+tags = {
+    "Q": "",
+    "A": "",
+    "AA": "",
+    "Addendum": "",
+    "Book": "",
+    "Page": "",
+    "Chapter": "",
+    "Tags": ""
+}
+
+for token, section in split_sections(contents):
+
+    emoji, token_type = token.groups()
+    if emoji == "✅":
+        pass
+    value = convert_math_delims(section.strip())
+    tags[token_type] = value
+
+    if token_type == "A":
+        print("Q:")
+        print(tags["Q"])
+        print("A:")
+        print(tags["A"])
+        print("Book:", tags["Book"])
+        print("Chapter:", tags["Chapter"])
+        print("Page:", tags["Page"])
+
+    print("-"*20)
